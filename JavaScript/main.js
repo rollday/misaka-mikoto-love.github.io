29091b07bc77fd03642b1a2377e1e530f6d8d3bc
roll-day <115356328+roll-day@users.noreply.github.com>
2022-10-29 16:58:00 +0800

    Add files via upload

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,47 +1,17 @@
-// JavaScript Document
-$(function() {
-    $(".userWrap").on("click", "li", function() {
-      var sId = $(this).data("id"); //获取data-id的值
-      window.location.hash = sId; //设置锚点
-      loadInner(sId);
-    });
+const initScroll = () => {
+  const root = document.querySelector('.marquee');
+  if (!root) return;
+  const ul = root.querySelector('ul');
+  ul.innerHTML += ul.innerHTML;
+};
 
-    function loadInner(sId) {
-      var sId = window.location.hash;
-      var pathn, i;
-      switch (sId) {
-        case "#center":
-          pathn = "./pages/single-page/s-index.html";
-          i = 0;
-          break;
-        case "#about-railgun":
-          pathn = "./pages/single-page/about-railgun.html";
-          i = 1;
-          break;
-        case "#links":
-          pathn = "./pages/single-page/links.html";
-          i = 2;
-          break;
-        case "#about":
-          pathn = "./pages/single-page/about.html";
-          i = 3;
-          break;
-		case "#need-help":
-          pathn = "./pages/single-page/need-help.html";
-          i = 4;
-          break;
-		case "#artalk":
-          pathn = "./pages/single-page/artalk.html";
-          i = 5;
-          break;
-        default:
-          pathn = "./pages/single-page/s-index.html";
-          i = 0;
-          break;
-      }
-      $("#content").load(pathn); //加载相对应的内容
-      $(".userMenu li").eq(i).addClass("current").siblings().removeClass("current"); //当前列表高亮
-    }
-    var sId = window.location.hash;
-    loadInner(sId);
-  });
+const init = () => {
+  initScroll();
+  let maxWidth = localStorage.getItem('maxWidth') === 'true';
+  if (maxWidth) {
+    document.querySelector('.container').classList.add('max-width');
+  }
+};
+
+addEventListener('DOMContentLoaded', init);
+console.log("%c 作者：roll-day(misaka-mikoto-love) %c https://github.com/roll-day ", "color: #fff; margin: 1em 0; padding: 5px 0; background: #E18393;", "margin: 1em 0; padding: 5px 0; background: #efefef;")
