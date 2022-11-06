09217a6a3beb6e73c63d9f502765768c9c3a53b2
roll-day <115356328+roll-day@users.noreply.github.com>
2022-11-06 09:09:46 +0800

    Add files via upload

---
+++ JavaScript/main.js
@@ -0,0 +1,2 @@
+addEventListener('DOMContentLoaded', init);
+console.log("%c 作者：roll-day(misaka-mikoto-love) %c https://github.com/roll-day ", "color: #fff; margin: 1em 0; padding: 5px 0; background: #E18393;", "margin: 1em 0; padding: 5px 0; background: #efefef;")
