b3487f6379990eddcf1e44c81730fb4ac49f2f31
DES(9e8e6620c8142a70dc2f2472ecab747f5787cf89943d2bac) <113221030+misaka-mikoto-love@users.noreply.github.com>
2022-12-01 12:15:27 +0800

    Create need.js

---
+++ pages/resource/need.js
@@ -0,0 +1,46 @@
+function toDouble(num) {
+  num >= 10 ? num = '' + num : num = '0' + num;
+  return num;
+}
+var main = $$('main'),
+  span = $$('main').getElementsByTagName('span')[0];
+
+
+function timer(year, month, day, hour, minute, seconds, elem) {
+  var hour = hour || 0,
+    minute = minute || 0,
+    seconds = seconds || 0;
+  var endTime = new Date();
+  endTime.setFullYear(parseInt(year)),
+    endTime.setMonth(parseInt(month) - 1),
+    endTime.setDate(parseInt(day)),
+    endTime.setHours(parseInt(hour)),
+    endTime.setMinutes(parseInt(minute)),
+    endTime.setSeconds(parseInt(seconds));
+  setTime();
+  setInterval(function () {
+    setTime()
+  }, 1000);
+
+  function setTime() {
+    var startTime = new Date();
+    var lengthTime = parseInt((startTime.getTime() - endTime.getTime()) / 1000);
+    var lSeconds = parseInt(lengthTime % 60),
+      lMinute = parseInt((lengthTime / 60) % 60),
+      lHour = Math.floor((lengthTime / 3600) % 24),
+      lDay = Math.floor(lengthTime / (24 * 3600));
+    span.innerHTML = lDay + '天' + toDouble(lHour) + '小时' + toDouble(lMinute) + '分钟' + toDouble(lSeconds) + '秒';
+  }
+}
+
+function $$(id) {
+  return document.getElementById(id);
+}
+timer(2022, 08, 07, 0, 0, 0, span);
+var _hmt = _hmt || [];
+(function () {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?e0a1048b7b0e93e7ecd0a7f9a1a81083";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
